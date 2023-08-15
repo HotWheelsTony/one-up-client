7c90ac3a00edbc65748e179cd7fdcbe7765ae039
Ford Leins <fordleins@gmail.com>
2023-08-15 14:46:23 +1000

    Make home page handle container styling, style account cards

--- src/app/accounts/account-card/account-card.component.ts
+++ src/app/accounts/account-card/account-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-account-card',
-  templateUrl: './account-card.component.html',
-  styleUrls: ['./account-card.component.css']
+    selector: 'app-account-card',
+    templateUrl: './account-card.component.html',
+    styleUrls: ['./account-card.component.css']
 })
 export class AccountCardComponent {
+    @Input()
+    displayName: string | null = null;
 
+    @Input()
+    value: number | null = null;
 }
