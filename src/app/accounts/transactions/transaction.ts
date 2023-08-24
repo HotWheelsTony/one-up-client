083e58a66666549d0794c0470c1fdafb40766dd9
Ford Leins <fordleins@gmail.com>
2023-08-24 23:58:48 +1000

    Read in transactions

---
+++ src/app/accounts/transactions/transaction.ts
@@ -0,0 +1,7 @@
+export class Transaction {
+    public status!: string;
+    public description!: string;
+    public message!: string;
+    public currency!: string;
+    public value!: string;
+}
--- src/app/accounts/transactions/transactions.component.ts
+++ src/app/accounts/transactions/transactions.component.ts
@@ -1,35 +1,60 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Account } from '../account';
 import { AccountsService } from '../accounts.service';
-import { map } from 'rxjs'
+import { Transaction } from './transaction';
 
 @Component({
     selector: 'app-transactions',
     templateUrl: './transactions.component.html',
     styleUrls: ['./transactions.component.css']
 })
-export class TransactionsComponent {
+export class TransactionsComponent implements OnInit {
 
     public account!: Account;
+    public transactions!: Transaction[];
 
 
-    constructor(private _accountsService: AccountsService, private _router: Router, private _activatedRoute: ActivatedRoute) {
+    constructor(private _accountsService: AccountsService, private _router: Router, private _activatedRoute: ActivatedRoute) { }
+
+    ngOnInit(): void {
         const id = this._activatedRoute.snapshot.paramMap.get('accountId');
         if (id) {
-            this._accountsService.getAccountById(id).subscribe(
-                (response) => {
-                    const acc = response.data;
-                    this.account = {
-                        id: acc.id,
-                        name: acc.attributes.displayName,
-                        value: acc.attributes.balance.value,
-                        currency: acc.attributes.balance.currencyCode,
-                    } as Account;
-                }
-            );
+            this.getAccountInfo(id);
+            this.getTransactions(id);
         }
+    }
 
+    private getAccountInfo(id: string) {
+        this._accountsService.getAccountById(id).subscribe(
+            (response) => {
+                const acc = response.data;
+                this.account = {
+                    id: acc.id,
+                    name: acc.attributes.displayName,
+                    value: acc.attributes.balance.value,
+                    currency: acc.attributes.balance.currencyCode,
+                } as Account;
+            }
+        );
+    }
+
+    private getTransactions(id: string) {
+        this._accountsService.getAccountTransactions(id).subscribe(
+            (response) => {
+                this.transactions = response.data.map((trans: any) => {
+                    return {
+                        id: trans.id,
+                        status: trans.attributes.status,
+                        message: trans.attributes.message,
+                        value: trans.attributes.amount.value,
+                        description: trans.attributes.description,
+                        currency: trans.attributes.amount.currencyCode,
+                    } as Transaction;
+                }) as Transaction[];
+                console.log(this.transactions);
+            }
+        );
     }
 
 
