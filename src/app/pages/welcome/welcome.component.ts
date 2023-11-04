4793ce69161777c5afa1c28c281ee33c242d8399
Ford Leins <fordleins@gmail.com>
2023-11-04 15:18:08 +1100

    Add loading to welcome screen

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
@@ -17,7 +18,7 @@ export class WelcomeComponent implements OnInit {
     public isToastOpen = false;
 
 
-    constructor(private _formBuilder: FormBuilder, private _authService: AuthService, private _router: Router, private _toastService: ToastService) {
+    constructor(private _loadingCtrl: LoadingController, private _formBuilder: FormBuilder, private _authService: AuthService, private _router: Router, private _toastService: ToastService) {
         this.tokenForm = this._formBuilder.group({
             token: ['', Validators.required],
         });
@@ -32,13 +33,31 @@ export class WelcomeComponent implements OnInit {
 
 
     public async onSubmit() {
+        this.showLoading(true);
+
         const token = this.tokenForm.value.token;
         const isTokenValid = await lastValueFrom(this._authService.validateToken(token));
 
+        this.showLoading(false);
+
         if (isTokenValid) {
             localStorage.setItem('token', token);
+            this._router.navigate(['accounts']);
         } else {
             this.showToast(true);
+        }
+    }
+
+
+    private async showLoading(show: boolean) {
+        if (show) {
+            const loading = await this._loadingCtrl.create({
+                message: 'Verifying token...',
+                spinner: 'crescent',
+            });
+            loading.present();
+        } else {
+            this._loadingCtrl.dismiss();
         }
     }
 
