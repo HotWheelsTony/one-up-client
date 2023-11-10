d9ce0f6530c7c9dc9c2259eeb929d82ed28756be
Ford Leins <fordleins@gmail.com>
2023-11-10 11:15:50 +1100

    Work on search screen

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-search',
     templateUrl: './search.component.html',
     styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit, OnDestroy {
+
+    public searchForm: FormGroup;
+
+    private _searchFormSubscription?: Subscription;
+
+    constructor(private _formBuilder: FormBuilder) {
+        this.searchForm = this._formBuilder.group({
+            query: [],
+            since: [],
+            until: [],
+            fromAmount: [],
+            toAmount: [],
+        });
+    }
 
 
-    constructor() { }
+    ngOnInit(): void {
+        this._searchFormSubscription = this.searchForm.valueChanges.subscribe(
+            () => {
+                //use values from form to call update and filter methods here
+                console.log(this.searchForm.value);
+
+            }
+        );
+    }
+
+
+    ngOnDestroy(): void {
+        this._searchFormSubscription?.unsubscribe();
+    }
+
 
 
     // public handleSearch(event: any) {
